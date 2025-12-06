a8bb1bdc9348061a147e4a9b38b94f29f5124925
Anisul Haque <anisulhaque773@gmail.com>
2025-12-06 02:26:40 +0100

    last ok for render

--- event_management/settings.py
+++ event_management/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', #for render
+    # 'whitenoise.middleware.WhiteNoiseMiddleware', #for render
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -61,8 +61,8 @@ ROOT_URLCONF = 'event_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # 'DIRS': [],
-        'DIRS': [BASE_DIR / 'templates'], #for render
+        'DIRS': [],
+        # 'DIRS': [BASE_DIR / 'templates'], #for render
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -146,12 +146,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles' #for render
+# STATIC_ROOT = BASE_DIR / 'staticfiles' #for render
 STATICFILES_DIRS = [
     BASE_DIR /'static'
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" # for render
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage" # for render
 
 
 MEDIA_URL = '/media/'
