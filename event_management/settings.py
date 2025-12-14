032d6d7d73b0c159853d82abf8d46e777e00e8fd
Anisul Haque <anisulhaque773@gmail.com>
2025-12-14 22:53:04 +0100

    root setting for render

--- event_management/settings.py
+++ event_management/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR / 'staticfiles' #for render
+STATIC_ROOT = BASE_DIR / 'staticfiles' #for render
 STATICFILES_DIRS = [
     BASE_DIR /'static'
 ]
